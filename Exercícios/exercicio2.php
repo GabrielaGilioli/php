be6ab8fd5578a965e292bbb6ee1f14b5ef130bf2
GabrielaGilioli <gabrielagilioli.senai@gmail.com>
2023-08-11 12:53:01 -0300

    feito

---
+++ Exercícios/exercicio2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe Geral</title>
+
+    <!-- CSS Interno -->
+    <style>
+        .destaque {color: blue}
+        .mesclado {color: green}
+    </style>
+
+</head>
+<body>
+    <h1 class="destaque">Trabalhando com PHP</h1>
+    <p>Programador: Gabriela Gilioli</p>
+    <hr>
+
+    <?php
+        // Geração de texto (string)
+        echo "Chama Sesi-Senai !";
+
+        /* Geração de texto estruturado (com tags, atributos) */
+        echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP. </h2>";
+        echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP. </h2>";
+
+    ?>
+
+    <h1 class="mesclado">HTML e PHP mesclados</h1>
+    <p>Parágrafos HTML</p>
+
+    <?php
+        $linguagem = "Linguagem PHP.";
+    ?>
+    <p>Parágrafo mesclando HTML com <?=$linguagem?> </p>
+</body>
+</html>
